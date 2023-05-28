49c6f9a3bdbc4f5a7bd8762a2d050b254ceb3546
GerejoiK <GerejoiK@protonmail.com>
2023-05-28 21:40:49 +0200

    add vbt 2009

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,8 +1,10 @@
+import vbt2009 from "../data/vbt2009.yaml";
 import vbt2010 from "../data/vbt2010.yaml";
 import vbt2011 from "../data/vbt2011.yaml";
 import vbt2012 from "../data/vbt2012.yaml";
 import vbt2013 from "../data/vbt2013.yaml";
 const turniere = {
+	...vbt2009,
 	...vbt2010,
 	...vbt2011,
 	...vbt2012,
