cce19f6a240c01f465671e96d24bbe6cad4bd372
GerejoiK <GerejoiK@protonmail.com>
2023-07-27 19:57:13 +0200

    put some stuff on the index page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<p>Ein nach Vollständigkeit strebendes Archiv aller Videobattleturniere.</p>
+<p>
+	Der Quellcode ist frei verfügbar und Pubilc Domain.<br />
+	<a href="https://github.com/gerejoik/vbt-archiv/blob/master/LICENSE">
+		<img alt="Unlicense" src="/license.svg" />
+	</a>
+</p>
+<p>
+	Für weitere Information besuche GitHub.<br />
+	<a href="https://github.com/gerejoik/vbt-archiv">
+		<img alt="GitHub" src="/github.svg" />
+	</a>
+</p>
+<p>
+	Entwickelt mit:<br />
+	<a href="https://kit.svelte.dev/">
+		<img alt="SvelteKit" src="/sveltekit.svg" />
+	</a>
+</p>
