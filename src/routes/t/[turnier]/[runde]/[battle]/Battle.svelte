b4cd9917cfbe4bc34102a328bb89a506d9ca0051
GerejoiK <GerejoiK@protonmail.com>
2025-12-14 14:35:15 +0000

    add archive.org

--- src/routes/t/[turnier]/[runde]/[battle]/Battle.svelte
+++ src/routes/t/[turnier]/[runde]/[battle]/Battle.svelte
@@ -24,7 +24,6 @@
 					src={v.url.href.replace("https://youtu.be", "https://www.youtube.com/embed")}
 					title=""
 					frameborder="0"
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 					allowfullscreen
 					loading="lazy"
 				></iframe>
@@ -32,6 +31,19 @@
 		{:else if v.url.hostname.includes("myspace") || v.url.hostname.includes("myvideo")}
 			<span title="Originalupload">{v.original ? "🌟" : ""}</span>
 			<a href={v.url.href}>{v.url.hostname}</a>
+		{:else if v.url.hostname.includes("archive.org")}
+			<details>
+				<summary><a href={v.url.href}>{v.url.href}</a></summary>
+				<iframe
+					width="640"
+					height="360"
+					src={v.url.href.replace("details", "embed")}
+					title=""
+					frameborder="0"
+					allowfullscreen
+					loading="lazy"
+				></iframe>
+			</details>
 		{:else}
 			<span title="Originalupload">{v.original ? "🌟" : ""}</span>
 			<a href={v.url.href}>{v.url.href}</a>
